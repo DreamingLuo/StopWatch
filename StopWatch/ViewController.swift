031064587d57bca287f950af30a14627cb216cd2
luogang <295837274@qq.com>
2016-12-30 13:28:44 +0800

    Initial Commit

---
+++ StopWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatch
+//
+//  Created by phiic on 16/12/30.
+//  Copyright © 2016年 luogang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
