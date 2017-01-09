6894477563ad36c7e507f858e05d219a7e7698dd
luogang <295837274@qq.com>
2017-01-09 13:55:58 +0800

    swift定时器
    仿照allenwong的30DaysofSwift自己实现的一个计时器

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,9 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TitleLab: UILabel!
+  
+    @IBOutlet weak var pauseBtn: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    
+    
+    var timer=NSTimer()
+    
+    var counter=0.0
+    
+    //开始按钮
+    @IBAction func startBtn(sender: UIButton) {
+        
+        startBtn.enabled=false
+        pauseBtn.enabled=true
+        
+        timer=NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+    
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    //停止按钮
+    @IBAction func pauseBtn(sender: UIButton) {
+        
+        
+        
+        timer.invalidate()
+        
+        startBtn.enabled=true
+        
+        pauseBtn.enabled=false
+        
+        
+        
+        
+    }
+    //重置按钮
+    @IBAction func resetBtn(sender: AnyObject) {
+        
+        
+        counter=0.0
+        
+        TitleLab.text=String(format: "%.1f",counter)
+        
+        
+        
+        
+    }
+    //时间更新方法
+    func updateTime()
+    {
+        
+        counter=counter+0.1
+    
+       TitleLab.text=String(format: "%.1f", counter)
+      
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
